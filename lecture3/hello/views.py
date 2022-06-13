81d5c381df8e8b975f0211089b85762519d8c08a
irynadvoretska <anjes@i.ua>
2022-06-13 12:00:19 +0200

    404

--- lecture3/hello/views.py
+++ lecture3/hello/views.py
@@ -3,6 +3,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world!")
+    return render(request, "hello/index.html")
 def iryna(request):
     return HttpResponse("Hello, Iryna!")
+def greet(request, name):
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize()
+        })
